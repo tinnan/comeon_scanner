b25b2274ef8a376fb620448f9e8f91dbf63eca36
tinnan <tin_nan@hotmail.com>
2017-02-28 00:46:39 +0700

    Initial commit

---
+++ scanner.py
@@ -0,0 +1,25 @@
+from lxml import html
+from util.selectors import *
+import requests
+
+
+pageId = 31304
+
+try:
+    page = requests.get("http://www.comeon-book.com/comeonv3/story.php?SID=" + str(pageId))
+except:
+    print("Something went wrong while the program trying to open the URL. Try again later.")
+    exit(1)
+
+tree = html.fromstring(page.content)
+title = TITLE_SELECTOR(tree)
+if title and title[0] is not None:
+    print("Title=" + title[0].text)
+else:
+    print("No title, assume that this story page does not actually exist.")
+    exit(1)
+
+chapterTable = CHAPTER_ROW_TBL_SELECTOR(tree)
+if chapterTable:
+    [print(e2.text) for e2 in CHAPTER_SELECTOR(chapterTable[0])]
+    [print(e3.text) for e3 in CHAPTER_DATE_SELECTOR(chapterTable[0])]
---
+++ util/selectors.py
@@ -0,0 +1,17 @@
+from lxml.cssselect import CSSSelector
+
+TITLE_SELECTOR = CSSSelector('table:nth-child(5) '
+                             'table:first-child '
+                             'table:first-child '
+                             'table:first-child '
+                             'tr:first-child '
+                             'td:first-child '
+                             'b:first-child')
+CHAPTER_ROW_TBL_SELECTOR = CSSSelector('table:nth-child(5) '
+                               'table:first-child '
+                               'table:first-child '
+                               'table:first-child '
+                               'tr:nth-child(2) '
+                               'table')
+CHAPTER_SELECTOR = CSSSelector('td:first-child > a > font')
+CHAPTER_DATE_SELECTOR = CSSSelector('td:nth-child(2)')
